90b972e644ca61b9751b5f5c903cccb0996ba0fc
bhddo <125513079+bhddo@users.noreply.github.com>
2023-07-10 07:01:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                    ];
+
+function getMenu(){
+var htmldata;
+//Completar o código
+}
+menuListArray = ["pizza de vegetariana"
+                     "pizza de frango",
+                     "pizza portugesa",
+                     "pizza quato queijos",
+                     "pizza de calabreza",
+                     "pizza extravaganza"]
+
+function addItem(){
+var htmldata;
+var item=document.getElementById("addItem").value;
+}
+function getMenu(){
+var htmldata="";
+menuListArray.sort();
+for(vari=0;i<menuListArray.legtn;i++){
+    innerHTML = htmldata+ menuListArray[i] + '<br>'
+}
+    document.getElementById("displayMenu").innerHTML = htmldata;
+
+}
+
+function addItem(){
+   var htmldata;
+   var imgtags='img id= <"im1" src= "images/pizzaimg.png"/>'
+   var item=document.getElementById("addten").value;
+   menuListArray.sort()
+   htmldata=""
+   for(vari=0;i<menuListArray.legtn;i++){
+    innerHTML = htmldata+ menuListArray[i] + '<br>';
+}
+    document.getElementById("displayaddMenu").innerHTML = htmldata;
+}
+
+function addTop{
+    var item.getElementById("addItem").value;
+    menuListArray.push(item);
+    addItem()
+}
